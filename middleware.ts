f215376f20e2e6d791edf5d1f1fa24693997579d
Aditya Singh varma <hackerboy5916@gmail.com>
2025-09-24 11:04:30 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,10 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * - /scan/* (QR code scan routes - public)
+     * - /r/* (short code routes - public)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|scan/|r/|.*.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
