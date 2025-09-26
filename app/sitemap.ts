e8206ce166ef923bf3591a55aaf7ef982f3bbbaa
Aditya Singh varma <hackerboy5916@gmail.com>
2025-09-26 19:49:58 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,26 @@
-import type { MetadataRoute } from "next"
+import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  const base = "https://www.qranalyticshub.space";
+
   return [
     {
       url: `${base}/`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
+      priority: 1.0,
+      images: [
+        {
+          loc: `${base}/qr_code.png`, // Image in /public folder
+          title: "QR Code Generate and Analytics(100% free)",
+        },
+      ],
     },
     {
       url: `${base}/dashboard`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
       priority: 0.6,
     },
-  ]
+  ];
 }
-
-
